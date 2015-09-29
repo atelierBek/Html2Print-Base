b46e77affbf786cc2418cbeb04dcccbe5254b5b2
Alexandre Leray <alexandre@stdin.fr>
2015-09-29 23:55:02 +0200

    La GUI garde en mémoire les options cochés lorsque le document est rechargé

--- js/gui.js
+++ js/gui.js
@@ -1,66 +1,96 @@
 (function($, undefined) {
     'use strict';
 
-    $('#viewport')[0].addEventListener("load", function(event) {
-        var doc = this.contentDocument.getElementsByTagName('html')[0];
+    var $viewport = $("#viewport")[0]
+        , $previewBtn = $('[name="preview"]')[0]
+        , $gridBtn = $('[name="grid"]')[0]
+        , $debugBtn = $('[name="debug"]')[0]
+        , $spreadBtn = $('[name="spread"]')[0]
+        , $zoomBtn = $('[name="zoom"]')[0]
+        , $pageBtn = $('[name="page"]')[0]
+        , $reloadBtn = $('#reload')[0]
+        , $printBtn = $('#print')[0]
+    ;
+
+    $viewport.addEventListener("load", function(event) {
+        var $doc = this.contentDocument.getElementsByTagName('html')[0]
         
-        $('[name="preview"]')[0].addEventListener("change", function(event) {
+        function switchPreview(event) {
             if(this.checked) {
-                doc.classList.add("preview");
-                doc.classList.remove("normal");
+                $doc.classList.add("preview");
+                $doc.classList.remove("normal");
             } else {
-                doc.classList.add("normal");
-                doc.classList.remove("preview");
+                $doc.classList.add("normal");
+                $doc.classList.remove("preview");
             }
-        });
+        }
 
-        $('[name="grid"]')[0].addEventListener("change", function(event) {
+        function switchGrid(event) {
+            console.log(event);
+            if (this.checked) {
+                $doc.classList.add("grid");
+            } else {
+                $doc.classList.remove("grid");
+            }
+        }
+
+        function switchDebug(event) {
             if(this.checked) {
-                doc.classList.add("grid");
+                $doc.classList.add("debug");
             } else {
-                doc.classList.remove("grid");
+                $doc.classList.remove("debug");
             }
-        });
+        }
 
-        $('[name="debug"]')[0].addEventListener("change", function(event) {
+        function switchSpread(event) {
             if(this.checked) {
-                doc.classList.add("debug");
+                $doc.classList.add("spread");
             } else {
-                doc.classList.remove("debug");
+                $doc.classList.remove("spread");
             }
-        });
+        }
 
-        $('[name="spread"]')[0].addEventListener("change", function(event) {
-            if(this.checked) {
-                doc.classList.add("spread");
-            } else {
-                doc.classList.remove("spread");
-            }
-        });
-
-        $('[name="zoom"]')[0].addEventListener("change", function(event) {
+        function setZoom(event) {
             var zoomLevel = this.value / 100;
-            var elt = doc.querySelector("#pages");
+            var elt = $doc.querySelector("#pages");
             
             elt.style.webkitTransform = "scale(" + zoomLevel + ")";
             elt.style.webkitTransformOrigin = "0 0";
-        });
+        }
 
-        $('[name="page"]')[0].addEventListener("change", function(event) {
+        function changePage(event) {
             var pageNumber = this.value - 1;
 
-            var target = doc.querySelectorAll('.paper')[pageNumber];
+            var target = $doc.querySelectorAll('.paper')[pageNumber];
             var offsetTop = target.offsetTop;
 
-            doc.querySelector('body').scrollTop = offsetTop;
-        });
+            $doc.querySelector('body').scrollTop = offsetTop;
+        }
 
-        $('#reload')[0].addEventListener("click", function(event) {
-            $("#viewport")[0].contentWindow.location.reload();
-        });
+        function reload(event) {
+           $viewport.contentWindow.location.reload();
+        }
 
-        $('#print')[0].addEventListener("click", function(event) {
-            $("#viewport")[0].contentWindow.print();
-        });
+        function print(event) {
+            $viewport.contentWindow.print();
+        }
+
+
+        $previewBtn.addEventListener("change", switchPreview);
+        $gridBtn.addEventListener("change", switchGrid);
+        $debugBtn.addEventListener("change", switchDebug);
+        $spreadBtn.addEventListener("change", switchSpread);
+        $zoomBtn.addEventListener("change", setZoom);
+        $pageBtn.addEventListener("change", changePage);
+        $reloadBtn.addEventListener("click", reload);
+        $printBtn.addEventListener("click", print);
+
+
+        switchPreview.bind($previewBtn)();
+        switchGrid.bind($gridBtn)();
+        switchDebug.bind($debugBtn)();
+        switchSpread.bind($spreadBtn)();
+        setZoom.bind($zoomBtn)();
+        //changePage.bind($pageBtn)();
     }, false);
 })(document.querySelectorAll.bind(document));
