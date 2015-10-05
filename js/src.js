89083beaca9ceda7d7c3f4c6569b8e2d1f67fce8
Antoine-Gelgon <antoine.gelgon@gmail.com>
2015-10-05 17:09:27 +0200

    ajout de l'article 10-le-monstre dans le scr.js

--- js/src.js
+++ js/src.js
@@ -2,15 +2,16 @@
     'use strict';
 
     var src =         {
-        "01_gares": "articles/01_gares/article.html", 
-        "02_quand-jetais-flamand": "articles/02_quand-jetais-flamand/article.html", 
-        "03_le-mac-gyver-des-campings": "articles/03_le-mac-gyver-des-campings/article.html", 
-        "04_alphabet": "articles/04_alphabet/article.html", 
-        "05_dialogue-interieur": "articles/05_dialogue-interieur/article.html", 
-        "06_le-silence-du-tram": "articles/06_le-silence-du-tram/article.html", 
-        "07_making-of": "articles/07_making-of/article.html", 
-        "08_verviers-epicentre-de-la-prochaine-revolution-musicale": "articles/08_verviers-epicentre-de-la-prochaine-revolution-musicale/article.html", 
-        "09_carte_postale": "articles/09_carte_postale/article.html"
+        "01_gares": "articles/01_gares/article.html",
+        "02_quand-jetais-flamand": "articles/02_quand-jetais-flamand/article.html",
+        "03_le-mac-gyver-des-campings": "articles/03_le-mac-gyver-des-campings/article.html",
+        "04_alphabet": "articles/04_alphabet/article.html",
+        "05_dialogue-interieur": "articles/05_dialogue-interieur/article.html",
+        "06_le-silence-du-tram": "articles/06_le-silence-du-tram/article.html",
+        "07_making-of": "articles/07_making-of/article.html",
+        "08_verviers-epicentre-de-la-prochaine-revolution-musicale": "articles/08_verviers-epicentre-de-la-prochaine-revolution-musicale/article.html",
+        "09_carte_postale": "articles/09_carte_postale/article.html",
+        "10_le-monstre-en-moi": "articles/10_le-monstre-en-moi/article.html"
     }
 
     var docs = new HTML2print.Docs;
