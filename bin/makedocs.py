944c5d57c6353d8871d0a4e11e441b6a2e0d1313
Alexandre Leray <alexandre@stdin.fr>
2015-10-08 13:30:23 +0200

    modifié le makedocs

--- bin/makedocs.py
+++ bin/makedocs.py
@@ -49,7 +49,7 @@ def get_client():
     return client
 
 
-def other():
+def main():
     #try:
         #shutil.rmtree('articles')
         ##shutil.rmtree('js')
@@ -57,8 +57,8 @@ def other():
         #pass
 
 
-    if not os.path.isdir("articles"):
-        os.mkdir('articles')
+    if not os.path.isdir("articles_new"):
+        os.mkdir('articles_new')
 
     client = get_client()
 
@@ -73,93 +73,25 @@ def other():
         slug = membership['article']['slug']
         url = membership['article']['url']
 
-        name = '%02d_%s' % (order, slug)
-        os.mkdir('articles/%s' % name)
+        name = slug
+        os.mkdir('articles_new/%s' % name)
 
         # Gets the template
         request = client.get(hostname + "/fr/publish/%s.tpl" % membership['id'])
-        f = codecs.open("articles/%s/article.html" % name, "w", "utf-8")
+        f = codecs.open("articles_new/%s/article.html" % name, "w", "utf-8")
         f.write(request.text)
         f.close()
 
         # Gets the css
-        os.mkdir('articles/%s/css' % name)
+        os.mkdir('articles_new/%s/css' % name)
         request = client.get(hostname + "/fr/publish/%s.css" % membership['id'])
-        f = codecs.open("articles/%s/css/generated.css" % name, "w", "utf-8")
+        f = codecs.open("articles_new/%s/css/generated.css" % name, "w", "utf-8")
         f.write(request.text)
         f.close()
 
         # Creates the style file
-        f = codecs.open("articles/%s/css/styles.css" % name, "w", "utf-8")
+        f = codecs.open("articles_new/%s/css/styles.css" % name, "w", "utf-8")
         f.write("/* Styles for %s */" % name)
-        f.close()
-
-
-def make_js():
-    from textwrap import dedent
-
-
-    def indent(lines, amount, ch=' '):
-        padding = amount * ch
-        return padding + ('\n' + padding).join(lines.split('\n'))
-
-    client = get_client()
-    hostname = 'https://medor.coop'
-    path = "/fr/publish/api/article-membership/?issue__id=1"
-    url = hostname + path
-
-    request = client.get(url)
-    src = {}
-
-    for membership in request.json():
-        order = membership['order']
-        slug = membership['article']['slug']
-
-        name = '%02d_%s' % (order, slug)
-        src[name] = 'articles/%s/article.html' % name
-
-    # Creates the src file
-    js = u"""\
-    ;(function(undefined) {
-        'use strict';
-
-        var src = %s
-
-        var docs = new HTML2print.Docs;
-        docs.initialize(src);
-    })();
-    """ % indent(json.dumps(src, sort_keys=True, indent=4), 8)
-    js = dedent(js)
-
-    if not os.path.isdir("js"):
-        os.mkdir('js')
-
-    f = codecs.open("js/src.js", "w", "utf-8")
-    f.write(js)
-    f.close()
-
-
-def fetch_stories():
-    client = get_client()
-    hostname = 'https://medor.coop'
-    path = "/fr/publish/api/article-membership/?issue__id=1"
-    url = hostname + path
-
-    request = client.get(url)
-
-    os.mkdir('stories')
-
-    for membership in request.json():
-        order = membership['order']
-        slug = membership['article']['slug']
-        url = membership['article']['url']
-
-        name = '%02d_%s' % (order, slug)
-
-        # Gets the template
-        request = client.get(hostname + "/fr/publish/%s.html" % membership['id'])
-        f = codecs.open("stories/%s.html" % name, "w", "utf-8")
-        f.write(request.text)
         f.close()
 
 
@@ -167,13 +99,6 @@ if __name__ == '__main__':
     import argparse
     import sys
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('subcommand')
-    args = parser.parse_args()
-
-    if args.subcommand == "make_js":
-        make_js()
-    elif args.subcommand == "fetch_stories":
-        fetch_stories()
+    main()
 
     sys.exit()
